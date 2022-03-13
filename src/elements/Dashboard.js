b1105ad9feec51f3b1779712dbdd74f0b986a98b
Kowatt <contact.kowatt@gmail.com>
2022-03-13 20:25:32 +0100

    Update Dashboard.js

--- src/elements/Dashboard.js
+++ src/elements/Dashboard.js
@@ -24,6 +24,10 @@ const Dashboard = () => {
     const [deletePopupState, setDeletePopupState] = useState(false)
     const [folderName, setFolderName] = useState("")
 
+    const [cardFormState, setCardFormState] = useState(false)
+    const [passFormState, setPassFormState] = useState(false)
+    const [noteFormState, setNoteFormState] = useState(false)
+
     const emptyFolder = {
         title: "No folder selected",
         id: null
@@ -145,9 +149,9 @@ const Dashboard = () => {
                         </Stack>
                         <Stack direction="column" spacing={1} className="folders-controls">
                             <Button variant="contained" color="error" size="small" fullWidth onClick={() => {if (currentFolder.id) setDeletePopupState(true)}}><DeleteForeverIcon /></Button>
-                            <Button variant="contained" size="small" fullWidth>+&nbsp;<CreditCardIcon /></Button>
-                            <Button variant="contained" size="small" fullWidth>+&nbsp;<VpnKeyIcon /></Button>
-                            <Button variant="contained" size="small" fullWidth>+&nbsp;<TextSnippetIcon /></Button>
+                            <Button variant="contained" size="small" fullWidth onClick={() => {setCardFormState(true)}}>+&nbsp;<CreditCardIcon /></Button>
+                            <Button variant="contained" size="small" fullWidth onClick={() => {setPassFormState(true)}}>+&nbsp;<VpnKeyIcon /></Button>
+                            <Button variant="contained" size="small" fullWidth onClick={() => {setNoteFormState(true)}}>+&nbsp;<TextSnippetIcon /></Button>
                         </Stack>
                     </div>
                 </Stack>
@@ -163,13 +167,61 @@ const Dashboard = () => {
                         </Stack>
                     </Box>
                 </FormPopup>
+
                 <FormPopup trigger={deletePopupState} setTrigger={setDeletePopupState}>
-                <Typography component="p" variant="p">
+                    <Typography component="p" variant="p">
                         Are you sure that you want to remove that folder and all its content ?
                     </Typography>
                     <Stack direction="row" spacing={1} style={{flex : 0}}>
                         <Button variant="contained" color="success" onClick={deleteFolder} fullWidth>Delete</Button>
                         <Button onClick={() => {setDeletePopupState(false)}} variant="contained" color="error" fullWidth>Cancel</Button> 
+                    </Stack>
+                </FormPopup>
+
+                <FormPopup trigger={cardFormState} setTrigger={setCardFormState}>
+                    <Typography component="h2" variant="h5">
+                        Add Credit card
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField margin="normal" required fullWidth id="card_name" label="Data Name" name="card_name" autoFocus/>
+                        <TextField margin="normal" required fullWidth id="card_number" label="Card number" name="card_number"/>
+                        <TextField margin="normal" required fullWidth id="card_date" label="Card date" name="card_date"/>
+                        <TextField margin="normal" required fullWidth id="card_ccv" label="Card CCV" name="card_ccv"/>
+                    </Box>
+                    <Stack direction="row" spacing={1} style={{flex : 0}}>
+                        <Button variant="contained" color="success" fullWidth>Add</Button>
+                        <Button onClick={() => {setCardFormState(false)}} variant="contained" color="error" fullWidth>Cancel</Button> 
+                    </Stack>
+                </FormPopup>
+
+                <FormPopup trigger={passFormState} setTrigger={setPassFormState}>
+                    <Typography component="h2" variant="h5">
+                        Add Password
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField margin="normal" required fullWidth id="pass_name" label="Data Name" name="pass_name" autoFocus/>
+                        <TextField margin="normal" required fullWidth id="pass_url" label="Website URL" name="pass_url"/>
+                        <TextField margin="normal" required fullWidth id="pass_user" label="Username" name="pass_user"/>
+                        <TextField margin="normal" required fullWidth id="pass" label="Password" name="pass"/>
+                    </Box>
+                    <Stack direction="row" spacing={1} style={{flex : 0}}>
+                        <Button variant="contained" color="success" fullWidth>Add</Button>
+                        <Button onClick={() => {setPassFormState(false)}} variant="contained" color="error" fullWidth>Cancel</Button> 
+                    </Stack>
+                </FormPopup>
+
+                <FormPopup trigger={noteFormState} setTrigger={setNoteFormState}>
+                    <Typography component="h2" variant="h5">
+                        Add Note
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField margin="normal" required fullWidth id="note_name" label="Data Name" name="note_name" autoFocus/>
+                        <TextField margin="normal" required fullWidth id="note_title" label="Note title" name="note_title"/>
+                        <TextField margin="normal" required fullWidth id="note" label="Note" name="note"/>
+                    </Box>
+                    <Stack direction="row" spacing={1} style={{flex : 0}}>
+                        <Button variant="contained" color="success" fullWidth>Add</Button>
+                        <Button onClick={() => {setNoteFormState(false)}} variant="contained" color="error" fullWidth>Cancel</Button> 
                     </Stack>
                 </FormPopup>
             </div>
