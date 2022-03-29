7ecadf3985f3a9b150b1c6bb9d97aaded4a47116
Kowatt <contact.kowatt@gmail.com>
2022-03-29 22:50:16 +0200

    update

--- src/elements/Dashboard.js
+++ src/elements/Dashboard.js
@@ -9,11 +9,18 @@ import AddIcon from '@mui/icons-material/Add'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import VpnKeyIcon from '@mui/icons-material/VpnKey';
-import TextSnippetIcon from '@mui/icons-material/TextSnippet';
-import CachedIcon from '@mui/icons-material/Cached';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+import VpnKeyIcon from '@mui/icons-material/VpnKey'
+import TextSnippetIcon from '@mui/icons-material/TextSnippet'
+import CachedIcon from '@mui/icons-material/Cached'
+import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
+import SaveIcon from '@mui/icons-material/Save'
+import Card from "./Card"
+import Pass from "./Pass"
+import Note from "./Note"
 
 const Dashboard = () => {
 
@@ -36,6 +43,10 @@ const Dashboard = () => {
     const [showCardForm, setShowCardForm] = useState(false)
     const [showPassForm, setShowPassForm] = useState(false)
     const [showNoteForm, setShowNoteForm] = useState(false)
+
+    const [showDeleteCard, setShowDeleteCard] = useState(false)
+    const [showDeletePass, setShowDeletePass] = useState(false)
+    const [showDeleteNote, setShowDeleteNote] = useState(false)
 
     const emptyCard = {
         _id: 0,
@@ -88,6 +99,8 @@ const Dashboard = () => {
     const [currentNote, setCurrentNote] = useState(emptyNote)
 
     const [passwordValue, setPasswordValue] = useState("")
+
+    const [showPassword, setShowPassword] = useState(false)
 
     const emptyFolder = {
         title: "No folder selected",
@@ -462,16 +475,76 @@ const Dashboard = () => {
         )
     )
 
+    async function deleteCard() {
+        const req = await fetch('http://localhost:4000/api/remove/data/card', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                cardId: currentCard._id
+            })
+        })
+
+        const data = await req.json()
+
+        if (data.status === 'ok') {
+            updateData()
+            setShowDeleteCard(false)
+        }
+    }
+
+    async function deletePass() {
+        const req = await fetch('http://localhost:4000/api/remove/data/pass', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                passId: currentPass._id
+            })
+        })
+
+        const data = await req.json()
+
+        if (data.status === 'ok') {
+            updateData()
+            setShowDeletePass(false)
+        }
+    }
+
+    async function deleteNote() {
+        const req = await fetch('http://localhost:4000/api/remove/data/note', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                noteId: currentNote._id
+            })
+        })
+
+        const data = await req.json()
+
+        if (data.status === 'ok') {
+            updateData()
+            setShowDeleteNote(false)
+        }
+    }
+
     const cardsList = cards.map (
-        (card) => (<Button key={card._id} variant="outlined" sx={{ margin: 1, justifyContent: "flex-start"}} onClick={() => {setCurrentCard(cards.find(item => item._id === card._id)); setShowCardForm(true)}}><CreditCardIcon />&nbsp;{card.name}</Button>)
+        (card) => (<Card key={card._id} card={card} setCurrentCard={setCurrentCard} cards={cards} setShowCardForm={setShowCardForm} deleteCard={setShowDeleteCard} />)
     )
 
     const passList = pass.map (
-        (passI) => (<Button key={passI._id} variant="outlined" sx={{ margin: 1, justifyContent: "flex-start"}} onClick={() => {setCurrentPass(pass.find(item => item._id === passI._id)); setShowPassForm(true)}}><VpnKeyIcon />&nbsp;{passI.name}</Button>)
+        (passI) => (<Pass key={passI._id} passI={passI} setCurrentPass={setCurrentPass} pass={pass} setShowPassForm={setShowPassForm} deletePass={setShowDeletePass} />)
     )
 
     const notesList = notes.map (
-        (note) => (<Button key={note._id} variant="outlined" sx={{ margin: 1, justifyContent: "flex-start"}} onClick={() => {setCurrentNote(notes.find(item => item._id === note._id)); setShowNoteForm(true)}}><TextSnippetIcon />&nbsp;{note.name}</Button>)
+        (note) => (<Note key={note._id} note={note} setCurrentNote={setCurrentNote} notes={notes} setShowNoteForm={setShowNoteForm} deleteNote={setShowDeleteNote} />)
     )
 
     function RenderActionBar(props) {
@@ -513,18 +586,36 @@ const Dashboard = () => {
         setEditNoteValueContent(currentNote.note)
     }
 
+    function RenderPasswordField(password) {
+        if (showPassword) {
+            return (
+                <Stack direction="row" spacing={1}>
+                    <TextField label="Password" InputProps={{readOnly: true}} fullWidth value={currentPass.pass}/>
+                    <Button onClick={() => {setShowPassword(false)}}><VisibilityOffIcon /></Button>
+                </Stack>
+            )
+        }
+
+        return (
+            <Stack direction="row" spacing={1}>
+                <TextField label="Password" type="password" InputProps={{readOnly: true}} fullWidth value={currentPass.pass}/>
+                <Button onClick={() => {setShowPassword(true)}}><RemoveRedEyeIcon /></Button>
+            </Stack>
+        )
+    }
+
     function RenderFormCard() {
         if (showCardAsEditMode === false) {
             return (
                 <FormPopup trigger={showCardForm} setTrigger={setShowCardForm}>
-                    <div onDoubleClick={handleEditCardDoubleClick}>
-                        <p>Double click on any field to enter edit mode</p>
                         <TextField margin="normal" label="Data name" InputProps={{readOnly: true}} fullWidth value={currentCard.name}/>
                         <TextField margin="normal" label="Card number" InputProps={{readOnly: true}} fullWidth value={currentCard.number}/>
                         <TextField margin="normal" label="Card date" InputProps={{readOnly: true}} fullWidth value={currentCard.date}/>
                         <TextField margin="normal" label="Card CCV" InputProps={{readOnly: true}} fullWidth value={currentCard.ccv}/>
-                        <Button onClick={() => {setShowCardForm(false)}} variant="contained" color="error" fullWidth>Close</Button>
-                    </div>
+                        <Stack direction="row" spacing={1}>
+                            <Button variant="contained" onClick={handleEditCardDoubleClick}><ModeEditIcon /></Button>
+                            <Button onClick={() => {setShowCardForm(false)}} variant="contained" color="error" fullWidth>Close</Button>
+                        </Stack>
                 </FormPopup>
             )
         }
@@ -535,9 +626,9 @@ const Dashboard = () => {
                 <TextField margin="normal" label="Card number" fullWidth value={editCardValueNumber} onChange={(e) => {setEditCardValueNumber(e.target.value)}}/>
                 <TextField margin="normal" label="Card date" fullWidth value={editCardValueDate} onChange={(e) => {setEditCardValueDate(e.target.value)}}/>
                 <TextField margin="normal" label="Card CCV" fullWidth value={editCardValueCCV} onChange={(e) => {setEditCardValueCCV(e.target.value)}}/>
-                <Stack direction="row">
-                    <Button onClick={() => {updateCard()}} sx={{margin: "3px"}} variant="contained" fullWidth color="success">Save</Button>
-                    <Button sx={{margin: "3px"}} onClick={() => {setShowCardAsEditMode(false)}} variant="contained" fullWidth color="error">Cancel</Button>
+                <Stack direction="row" spacing={1}>
+                    <Button onClick={() => {updateCard()}} variant="contained" color="success"><SaveIcon /></Button>
+                    <Button onClick={() => {setShowCardAsEditMode(false)}} variant="contained" fullWidth color="error">Cancel</Button>
                 </Stack>
             </FormPopup>
         )
@@ -547,14 +638,14 @@ const Dashboard = () => {
         if (showPassAsEditMode === false) {
             return (
                 <FormPopup trigger={showPassForm} setTrigger={setShowPassForm}>
-                    <div onDoubleClick={handleEditPassDoubleClick}>
-                        <p>Double click on any field to enter edit mode</p>
-                        <TextField margin="normal" label="Data name" InputProps={{readOnly: true}} fullWidth value={currentPass.name}/>
-                        <TextField margin="normal" label="Website" InputProps={{readOnly: true}} fullWidth value={currentPass.url}/>
-                        <TextField margin="normal" label="Username" InputProps={{readOnly: true}} fullWidth value={currentPass.user}/>
-                        <TextField margin="normal" label="Password" InputProps={{readOnly: true}} fullWidth value={currentPass.pass}/>
-                        <Button onClick={() => {setShowPassForm(false)}} fullWidth variant="contained" color="error">Close</Button>
-                    </div>
+                    <TextField margin="normal" label="Data name" InputProps={{readOnly: true}} fullWidth value={currentPass.name}/>
+                    <TextField margin="normal" label="Website" InputProps={{readOnly: true}} fullWidth value={currentPass.url}/>
+                    <TextField margin="normal" label="Username" InputProps={{readOnly: true}} fullWidth value={currentPass.user}/>
+                    {RenderPasswordField()}
+                    <Stack direction="row" spacing={1}>
+                        <Button variant="contained" onClick={handleEditPassDoubleClick}><ModeEditIcon /></Button>
+                        <Button onClick={() => {setShowPassForm(false)}} variant="contained" color="error" fullWidth>Close</Button>
+                    </Stack>
                 </FormPopup>
             )
         }
@@ -565,9 +656,9 @@ const Dashboard = () => {
                 <TextField margin="normal" label="Website" fullWidth value={editPassValueUrl} onChange={(e) => {setEditPassValueUrl(e.target.value)}}/>
                 <TextField margin="normal" label="Username" fullWidth value={editPassValueUser} onChange={(e) => {setEditPassValueUser(e.target.value)}}/>
                 <TextField margin="normal" label="Password" fullWidth value={editPassValuePass} onChange={(e) => {setEditPassValuePass(e.target.value)}}/>
-                <Stack direction="row">
-                    <Button onClick={() => {updatePass()}} sx={{margin: "3px"}} variant="contained" fullWidth color="success">Save</Button>
-                    <Button onClick={() => {setShowPassAsEditMode(false)}} sx={{margin: "3px"}} fullWidth variant="contained" color="error">Cancel</Button>
+                <Stack direction="row" spacing={1}>
+                    <Button onClick={() => {updatePass()}} variant="contained" color="success"><SaveIcon /></Button>
+                    <Button onClick={() => {setShowPassAsEditMode(false)}} variant="contained" fullWidth color="error">Cancel</Button>
                 </Stack>
             </FormPopup>
         )
@@ -577,13 +668,13 @@ const Dashboard = () => {
         if (showNoteAsEditMode === false) {
             return (
                 <FormPopup trigger={showNoteForm} setTrigger={setShowNoteForm}>
-                    <div onDoubleClick={handleEditNoteDoubleClick}>
-                        <p>Double click on any field to enter edit mode</p>
-                        <TextField margin="normal" label="Data name" InputProps={{readOnly: true}} fullWidth value={currentNote.name}/>
-                        <TextField margin="normal" label="Title" InputProps={{readOnly: true}} fullWidth value={currentNote.title}/>
-                        <TextField margin="normal" multiline label="Content" InputProps={{readOnly: true}} fullWidth value={currentNote.note}/>
-                        <Button onClick={() => {setShowNoteForm(false)}} fullWidth variant="contained" color="error">Close</Button>
-                    </div>
+                    <TextField margin="normal" label="Data name" InputProps={{readOnly: true}} fullWidth value={currentNote.name}/>
+                    <TextField margin="normal" label="Title" InputProps={{readOnly: true}} fullWidth value={currentNote.title}/>
+                    <TextField margin="normal" multiline label="Content" InputProps={{readOnly: true}} fullWidth value={currentNote.note}/>
+                    <Stack direction="row" spacing={1}>
+                        <Button variant="contained" onClick={handleEditNoteDoubleClick}><ModeEditIcon /></Button>
+                        <Button onClick={() => {setShowNoteForm(false)}} variant="contained" color="error" fullWidth>Close</Button>
+                    </Stack>
                 </FormPopup>
             )
         }
@@ -593,9 +684,9 @@ const Dashboard = () => {
                 <TextField margin="normal" label="Data name" fullWidth value={editNoteValueName} onChange={(e) => {setEditNoteValueName(e.target.value)}}/>
                 <TextField margin="normal" label="Title" fullWidth value={editNoteValueTitle} onChange={(e) => {setEditNoteValueTitle(e.target.value)}}/>
                 <TextField margin="normal" multiline label="Content" fullWidth value={editNoteValueContent} onChange={(e) => {setEditNoteValueContent(e.target.value)}}/>
-                <Stack direction="row">
-                    <Button onClick={() => {updateNote()}} sx={{margin: "3px"}} variant="contained" fullWidth color="success">Save</Button>
-                    <Button onClick={() => {setShowNoteAsEditMode(false)}} sx={{margin: "3px"}} fullWidth variant="contained" color="error">Cancel</Button>
+                <Stack direction="row" spacing={1}>
+                    <Button onClick={() => {updateNote()}} variant="contained" color="success"><SaveIcon /></Button>
+                    <Button onClick={() => {setShowNoteAsEditMode(false)}} variant="contained" fullWidth color="error">Cancel</Button>
                 </Stack>
             </FormPopup>
         )
@@ -609,7 +700,7 @@ const Dashboard = () => {
         <div>
             <Navbar />
             <div className="dashboard">
-                <Typography component="h4" variant="h4">
+                <Typography component="h4" variant="h4" sx={{marginTop: "4px"}}>
                     {username}'s dashboard
                 </Typography>
                 <Stack direction="row" spacing={1} className="stack">
@@ -702,6 +793,30 @@ const Dashboard = () => {
                             <Button onClick={() => {setNoteFormState(false)}} variant="contained" color="error" fullWidth>Cancel</Button> 
                         </Stack>
                     </Box>
+                </FormPopup>
+
+                <FormPopup trigger={showDeleteCard} setTrigger={setShowDeleteCard}>
+                    <p>Are you sure that you really want to permanently remove this data ?</p>
+                    <Stack direction="row" spacing={1}>
+                        <Button onClick={() => {deleteCard()}} color="success" fullWidth variant="contained">Delete</Button>
+                        <Button onClick={() => {setShowDeleteCard(false)}} color="error" fullWidth variant="contained">Cancel</Button>
+                    </Stack>
+                </FormPopup>
+
+                <FormPopup trigger={showDeletePass} setTrigger={setShowDeletePass}>
+                    <p>Are you sure that you really want to permanently remove this data ?</p>
+                    <Stack direction="row" spacing={1}>
+                        <Button onClick={() => {deletePass()}} color="success" fullWidth variant="contained">Delete</Button>
+                        <Button onClick={() => {setShowDeletePass(false)}} color="error" fullWidth variant="contained">Cancel</Button>
+                    </Stack>
+                </FormPopup>
+
+                <FormPopup trigger={showDeleteNote} setTrigger={setShowDeleteNote}>
+                    <p>Are you sure that you really want to permanently remove this data ?</p>
+                    <Stack direction="row" spacing={1}>
+                        <Button onClick={() => {deleteNote()}} color="success" fullWidth variant="contained">Delete</Button>
+                        <Button onClick={() => {setShowDeleteNote(false)}} color="error" fullWidth variant="contained">Cancel</Button>
+                    </Stack>
                 </FormPopup>
 
                 {RenderFormCard()} {/* Called this way to avoid Component update at each subcomponent change (textfields) */}
